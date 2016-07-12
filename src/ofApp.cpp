7d48c2d053ab3358187efc0532734378b418f722
ReallyRad <comarthur.pointeau@gmail.com>
2016-07-12 13:28:03 +0200

    creating random data for eda

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,86 +4,78 @@
 void ofApp::setup(){
 	TCP.setMessageDelimiter("\n");
 	TCP.setup(5555);
+	started = false;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	
-	
-	if (TCP.isClientConnected(0)) {		
+	//si hay algun cliente conectado
+	if (TCP.isClientConnected(0)) {	
+		//recibir mensaje del cliente
 		string str = TCP.receive(0);
 		ofxJSONElement element = new ofxJSONElement;
+		
+		//look for start/stop messages
 		if (str != "" && element.parse(str)) {
 			if (element["COMMAND"].asString() == "start") {
 				cout << "received start" << endl;
 				TCP.send(0,"{ \"STATUS\": \"OK\"}");
+				started = true;
 			}
 
 			if (element["COMMAND"].asString() == "stop") {
 				cout << "received stop" << endl;
 				TCP.send(0, "{ \"STATUS\": \"OK\"}");
+				started = false;
 			}
 		}
+		else {
+			//cout << "error receiving message" << endl;
+		}
+	
+		if (started) {
+
+			//every 500ms, eda_fasic and eda_tonic, 1*8 samples
+			if (ofGetElapsedTimeMillis() - timer_eda > 500) {
+				//reset the timer
+				timer_eda = ofGetElapsedTimeMillis();
+				
+				//use the template eda json file
+				ofxJSONElement e = new ofxJSONElement();
+				std::string file = "eda.json";
+				bool parsingSuccessful = e.open(file);
+				
+				if (parsingSuccessful) {
+					for (int i = 0; i < 2; i++) {
+						//for each eda signal, set 8 random values and the timestamp
+						for (int j = 0; j < 8; j++) e["DATA"][i]["Values"][j] = ofRandom(1);						
+						e["DATA"][i]["Timestamp"] = ofGetElapsedTimeMillis();
+					}
+					
+					//save to file to view results
+					e.save("test.json", true);
+					//send to client
+					TCP.send(0,e.getRawString());
+				}
+				
+			}
+
+			//every 31.25ms, eeg_alpha, eeg_beta, eeg_theta, 4*8 samples
+			if (ofGetElapsedTimeMillis() - timer_eeg > 31) {
+				timer_eeg = ofGetElapsedTimeMillis();
+
+			}
+
+			//every 1000 ms, ecg_hr, 1 sample
+			if (ofGetElapsedTimeMillis() - timer_ecg > 1000) {
+				timer_ecg = ofGetElapsedTimeMillis();
+			}
+
+
+			//randomly, adquisition error
+		}
+	
 	}
 		
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
